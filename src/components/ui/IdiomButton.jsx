bacd8fbedcb8522c8e32c4bd1b78293df14db148
Alfonso Ravelo Gil <al2367.arg@gmail.com>
2025-02-24 23:35:07 +0100

    feat: add transition and max hight

--- src/components/ui/IdiomButton.jsx
+++ src/components/ui/IdiomButton.jsx
@@ -4,14 +4,16 @@ const IdiomButton = () => {
     const [idiom, setIdiom] = useState("ES");
 
     return (
-        <button className="px-4 py-2 hover:bg-gray-100">
-            {idiom === "ES" 
-            ? <span className="font-black">ES</span>
-            : <span className="font-normal">ES</span>} 
-            / 
-            {idiom === "EN" 
-            ? <span className="font-black">EN</span>
-            : <span className="font-normal">EN</span>}
+        <button className="px-4 py-2 h-[40px]">
+            <div className="hover:text-lg transition-all">
+                {idiom === "ES" 
+                ? <span className="font-black">ES</span>
+                : <span className="font-normal">ES</span>} 
+                / 
+                {idiom === "EN" 
+                ? <span className="font-black">EN</span>
+                : <span className="font-normal">EN</span>}
+            </div>
         </button>
     );
 };
