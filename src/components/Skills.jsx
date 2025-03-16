1fe9f1a619a6427d7d2861c70dcd6a960caa33a2
Alfonso Ravelo Gil <al2367.arg@gmail.com>
2025-03-16 03:22:41 +0100

    feat: add skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,28 @@
+import { useLoadJsonDB } from '../contexts/LoadJsonDBContext';
+import SkillsCard from './ui/skillsCard';
+
+const Skills = () => {
+    const { resumeData, loading} = useLoadJsonDB();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <section className="container mx-auto px-6 py-4">
+            <header>
+                <h1 className="text-3xl mx-auto text-center m-5">{resumeData.skills.title.split(' ')[0]} <span className="font-extrabold">{resumeData.skills.title.split(' ')[1]}</span></h1>
+            </header>
+
+            <ul className="grid grid-cols-2 gap-4 max-w-7xl mx-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md:gap-8">
+                {resumeData.skills.skillArray.map((skill) => (
+                    <li key={skill.name}>
+                        <SkillsCard name={skill.name} svg={skill.svg} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+export default Skills;
